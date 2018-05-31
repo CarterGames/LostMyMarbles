3bb6fd0b16c4913a6be21d2a401166211ae7ef54
Unknown <jmc@devj.uk>
2018-05-31 21:22:40 +0100

    Marble Movement Added
    The marble can now move around, however there is friction of sorts when changing direction and the marble doesn't slow to a stop when force is not added which is needed soon.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,36 +5,48 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    public float speed;
-    public Vector3 MoveDirection;
+    public float MoveSpeed;
 
-    private Vector3 Pos;
+    private Rigidbody RB;
 
-
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
-		
+        RB = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-		
-        if (Input.GetKey("a"))
+
+        if (Input.GetKey("w"))
         {
-            Move(MoveDirection.x, 0);
+            Move(MoveSpeed, 0, 0);
+        }
+        if (Input.GetKey("s"))
+        {
+            Move(-MoveSpeed, 0, 0);
         }
 
-	}
+
+        if (Input.GetKey("a"))
+        {
+            Move(0, 0, MoveSpeed);
+        }
+        if (Input.GetKey("d"))
+        {
+            Move(0, 0, -MoveSpeed);
+        }
+    }
 
     // Moving the marble around the scene
-    void Move(float DirectionX, float DirectionZ)
+    void Move(float DirectionX, float DirectionY, float DirectionZ)
     {
-        // Moves the player based on the inputted X & Z vaules, Doesn't allow jump movement on this function
-        //transform.Translate(DirectionX * Time.deltaTime, MoveDirection.y, DirectionZ * Time.deltaTime);
-        Pos = new Vector3(DirectionX * Time.deltaTime, MoveDirection.y, DirectionZ * Time.deltaTime);
+        RB.AddTorque(DirectionX, DirectionY, DirectionZ);
+    }
 
-        //transform.SetPositionAndRotation()
+    void Slow()
+    {
+        RB.AddTorque(0, 0, 0);
     }
 }
