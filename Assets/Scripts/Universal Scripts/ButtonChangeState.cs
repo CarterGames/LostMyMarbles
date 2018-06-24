010f95ab6cf5fd25b031a1d0aea912d52f83fabc
Jonathan <jmc@devj.uk>
2018-06-24 12:34:05 +0100

    Dev - Main Menu & Level Texturing
    Done some work on the main menu, which does work. Though the world does need a skybox to make it look good. The first level needs a better floor texture and the floor itself needs a physics material to make the marble bounce a bit more with the surface.

---
+++ Assets/Scripts/Universal Scripts/ButtonChangeState.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonChangeState : MonoBehaviour
+{
+
+	public GameObject MenuManager;
+	public GameStates state;
+
+	private MenuManager Manager;
+
+	// Use this for initialization
+	void Start ()
+	{
+		Manager = MenuManager.GetComponent<MenuManager>();
+	}
+
+	public void SwapState()
+	{
+		Manager.LoadCurrentScene(state);
+	}
+}
---
+++ Assets/Scripts/Universal Scripts/MenuManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public enum GameStates
+{
+	Menu,
+	LevelSelect,
+	Level1,
+	Credits,
+	Quit,
+};
+
+
+public class MenuManager : MonoBehaviour
+{
+
+	public GameStates E_GameStates;
+
+
+	public void Awake()
+	{
+		DontDestroyOnLoad(gameObject);
+		Initial();
+	}
+
+	private void Initial()
+	{
+		LoadCurrentScene(E_GameStates);
+	}
+
+
+	public void LoadCurrentScene(GameStates state)
+	{
+		switch(state)
+		{
+			case GameStates.Menu:
+				SceneManager.LoadScene("Menu");
+				break;
+
+			case GameStates.LevelSelect:
+				SceneManager.LoadScene("LevelSelect");
+				break;
+
+			case GameStates.Level1:
+				SceneManager.LoadScene("Level1");
+				break;
+
+			case GameStates.Credits:
+				SceneManager.LoadScene("Credits");
+				break;
+
+			case GameStates.Quit:
+				Application.Quit();
+				break;
+
+			default:
+				break;
+		}
+	}
+
+	void Update()
+	{
+		if (Input.GetButton("Cancel"))
+		{
+			LoadCurrentScene(GameStates.Quit);
+		}
+	}
+}
