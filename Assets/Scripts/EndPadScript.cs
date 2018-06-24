cf93c09bd278b2bec2a12268c3c44606a5c5ffb4
Jonathan <jmc@devj.uk>
2018-06-24 09:58:06 +0100

    More Scripting
    Mainly more scripting, finished the TImer script to be a universal script with getters and setters in it. ALso started the End Pad Script with freezing the marble and using the TImer Script.

---
+++ Assets/Scripts/EndPadScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndPadScript : MonoBehaviour
+{
+	// Marble object so it can be freezed
+	[Header("Player Object")]
+	public GameObject Marble;
+
+	public TimerScript TimeScript;
+
+	// Rigidbody for the player, just to save a bit of space in the code
+	private Rigidbody PlayerRB;
+
+	// Use this for initialization
+	void Start ()
+	{
+		// Sets PlayerRB to the players rigid body component
+		PlayerRB = Marble.GetComponent<Rigidbody>();
+	}
+
+	void Update()
+	{
+		if (TimeScript.GetTimer() > 1.5f)
+		{
+			// Do End Stuff
+			Debug.Log("End");
+		}
+	}
+
+	// When the marble collides with the end pad trigger volume
+	private void OnTriggerEnter(Collider other)
+	{
+		TimeScript.SetTimer(0);
+		TimeScript.SetStartTimer(true);
+
+		PlayerRB.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezePositionZ;     // Freezes the player
+	}
+}
---
+++ Assets/Scripts/Universal Scripts/TimerScript.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//
+//	Universal Timer Script, with getters and setters
+//
+
+public class TimerScript : MonoBehaviour
+{
+
+	[Header("Timer Variables")]
+	public float Timer;			// Timer float
+	public bool StartTimer;		// Bool to start or stop the timer
+
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		// If the Start Timer Bool is true, then
+		if (StartTimer == true)
+		{
+			// Start counting up, by 1
+			Timer += 1 * Time.deltaTime;
+		}
+		else
+		{
+			// DO Nothing
+		}
+	}
+
+
+
+	//
+	//
+	//			Getter's 
+	//
+	//
+
+	
+	// Get the Timer Float
+	public float GetTimer()
+	{
+		return Timer;
+	}
+
+	// Get the Start Timer Bool
+	public bool GetStartTimer()
+	{
+		return StartTimer;
+	}
+
+
+
+	//
+	//
+	//			Setter's
+	//
+	//
+
+
+
+	// Sets the Timer float to the input
+	public void SetTimer(float input)
+	{
+		Timer = input;
+		return;
+	}
+
+	// Sets the StartTimer bool to the input
+	public void SetStartTimer(bool input)
+	{
+		StartTimer = input;
+		return;
+	}
+}
