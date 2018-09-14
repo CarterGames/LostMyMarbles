963c52d0de338d84d44b35ede6a5dfb91072a4bd
Jonathan <jmc@devj.uk>
2018-09-14 13:34:40 +0100

    Menu Changer Made
    Made a universal Ui text & button changer for menu buttons to change without changing scene. More can be added if need to this script.

---
+++ Assets/Scripts/Universal Scripts/CanvasChanger.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//
+//
+//			A universal script to change parts of a menu, by disable items in a canvas
+//
+//
+
+public class CanvasChanger : MonoBehaviour
+{
+
+	[Header("Elements in the Canvas to be Disabled")]
+	public Text[] TextDisable;
+	public Button[] ButtonDisable;
+
+	[Header("Elements in the Canvas to be Enabled")]
+	public Text[] TextEnable;
+	public Button[] ButtonEnable;
+
+
+
+	// Function to disable items
+	public void CanvasElementsDisable()
+	{
+
+		foreach(Text text in TextDisable)
+		{
+			text.enabled = false;
+		}
+
+		foreach(Button button in ButtonDisable)
+		{
+			button.gameObject.SetActive(false);
+		}
+	}
+
+
+	// Function to enable items
+	public void CanvasElementsEnable()
+	{
+
+		foreach (Text text in TextEnable)
+		{
+			text.enabled = true;
+		}
+
+		foreach (Button button in ButtonEnable)
+		{
+			button.gameObject.SetActive(true);
+		}
+
+	}
+
+}
