a26d7cfc8e8e2fe607fc98e3bb41589854064093
Jonathan <jmc@devj.uk>
2019-05-25 09:41:22 +0100

    Started Work on Level Save

--- Assets/Scripts/LevelTimerScript.cs
+++ Assets/Scripts/LevelTimerScript.cs
@@ -2,16 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LevelTimerScript : MonoBehaviour
 {
+	internal string PlayerName;
 	internal bool RunTimer;
 	private float Timer;
 	private Text TimerText;
+	private SaveScript Scores;
 
 	// Start is called before the first frame update
 	void Start()
     {
+		Scores = FindObjectOfType<SaveScript>();
 		TimerText = GetComponentInChildren<Text>();
     }
 
@@ -40,5 +44,53 @@ public class LevelTimerScript : MonoBehaviour
 	internal void StopTimer()
 	{
 		RunTimer = false;
+		UpdateLevelScores();
+	}
+
+	internal float GetTmer()
+	{
+		return Timer;
+	}
+
+	private void UpdateLevelScores()
+	{
+		LevelScores Changes = new LevelScores();
+
+		for (int i = 0; i < Scores.PerLevelData.Count; i++)
+		{
+			if (Scores.PerLevelData[i].LevelName == SceneManager.GetActiveScene().name)
+			{
+				Changes = Scores.PerLevelData[i];
+			}
+		}
+
+		Changes.LastTime = Timer;
+		
+		if (Timer > Changes.BestTime)
+		{
+			Changes.BestTime = Timer;
+			Changes.BestTimeName = PlayerName;
+		}
+		else if (Timer > Changes.SecondBestTime)
+		{
+			Changes.SecondBestTime = Timer;
+			Changes.SecondBestName = PlayerName;
+		}
+		else if (Timer > Changes.ThirdBestTime)
+		{
+			Changes.ThirdBestTime = Timer;
+			Changes.ThirdBestName = PlayerName;
+		}
+
+		// Sets the new changes, in theory
+		for (int i = 0; i < Scores.PerLevelData.Count; i++)
+		{
+			if (Scores.PerLevelData[i].LevelName == SceneManager.GetActiveScene().name)
+			{
+				Scores.PerLevelData[i] = Changes;
+			}
+		}
+
+		Scores.SaveData();
 	}
 }
--- Assets/Scripts/SaveScript.cs
+++ Assets/Scripts/SaveScript.cs
@@ -1,36 +1,95 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 // Save Script Namespaces
 using System;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
 
 
+public struct LevelScores
+{
+	public float BestTime;
+	public string BestTimeName;
+
+	public float SecondBestTime;
+	public string SecondBestName;
+
+	public float ThirdBestTime;
+	public string ThirdBestName;
+
+	public float LastTime;
+	public float StarTime;
+
+	public string LevelName;
+}
+
+
 public class SaveScript : MonoBehaviour
 {
+	public List<LevelScores> PerLevelData = new List<LevelScores>(20);
 
-	public struct LevelData
+	private void Start()
 	{
-		float LevelNo;
-		float StarTime;
-		LevelScores LevelScore;
+		//if (File.Exists(Application.persistentDataPath + "/gamedata.ini"))
+		//{
+		//	LoadData();
+		//}
+		//else
+		//{
+		//	CreateFile();
+		//}
 	}
 
-	public void SaveGame()
+
+	public void CreateFile()
+	{
+		FileStream DataFile;
+		DataFile = File.Create(Application.persistentDataPath + "/gamedata.ini");
+		DataFile.Close();
+	}
+
+	public void SaveData()
 	{
 		// does stuff with binrary one assumes :)
 		BinaryFormatter BinFormat = new BinaryFormatter();
 
+		FileStream DataFile;
+
 		// creates a file in the data path /C/users/appdata/......
-		FileStream DataFile = File.Create(Application.persistentDataPath + "/gamedata.dat");
+		if (!File.Exists(Application.persistentDataPath + "/gamedata.ini"))
+		{
+			DataFile = File.Create(Application.persistentDataPath + "/gamedata.ini");
+		}
+		else
+		{
+			DataFile = File.OpenWrite(Application.persistentDataPath + "/gamedata.ini");
+		}
 
 		// creates an instance of the game data class...
 		GameData Data = new GameData();
 
 		// populating the new instance with the current values in the game
-
+		Data.Level1 = PerLevelData[0];
+		Data.Level2 = PerLevelData[1];
+		Data.Level3 = PerLevelData[2];
+		Data.Level4 = PerLevelData[3];
+		Data.Level5 = PerLevelData[4];
+		Data.Level6 = PerLevelData[5];
+		Data.Level7 = PerLevelData[6];
+		Data.Level8 = PerLevelData[7];
+		Data.Level9 = PerLevelData[8];
+		Data.Level10 = PerLevelData[9];
+		Data.Level11 = PerLevelData[10];
+		Data.Level12 = PerLevelData[11];
+		Data.Level13 = PerLevelData[12];
+		Data.Level14 = PerLevelData[13];
+		Data.Level15 = PerLevelData[14];
+		Data.Level16 = PerLevelData[15];
+		Data.Level17 = PerLevelData[16];
+		Data.Level18 = PerLevelData[17];
+		Data.Level19 = PerLevelData[18];
+		Data.Level20 = PerLevelData[19];
 
 		// Converts to binrary, using the data from the data thingy in a data file
 		BinFormat.Serialize(DataFile, Data);
@@ -40,15 +99,15 @@ public class SaveScript : MonoBehaviour
 	}
 
 
-	public void LoadGame()
+	public void LoadData()
 	{
 		// checks to see if the file exsists, duh...
-		if (File.Exists(Application.persistentDataPath + "/gamedata.dat"))
+		if (File.Exists(Application.persistentDataPath + "/gamedata.ini"))
 		{
 			BinaryFormatter BinFormat = new BinaryFormatter();
 
 			// Makes a local file with the file in the location and opens it :)
-			FileStream DataFile = File.Open(Application.persistentDataPath + "/gamedata.dat", FileMode.Open);
+			FileStream DataFile = File.Open(Application.persistentDataPath + "/gamedata.ini", FileMode.Open);
 
 			// converts the file to readable thingys :) ( "unbinraryfys" the file )
 			GameData Data = (GameData)BinFormat.Deserialize(DataFile);
@@ -57,17 +116,52 @@ public class SaveScript : MonoBehaviour
 			DataFile.Close();
 
 			// Sets the values to the values that were in the file
-
+			PerLevelData[0] = Data.Level1;
+			PerLevelData[1] = Data.Level2;
+			PerLevelData[2] = Data.Level3;
+			PerLevelData[3] = Data.Level4;
+			PerLevelData[4] = Data.Level5;
+			PerLevelData[5] = Data.Level6;
+			PerLevelData[6] = Data.Level7;
+			PerLevelData[7] = Data.Level8;
+			PerLevelData[8] = Data.Level9;
+			PerLevelData[9] = Data.Level10;
+			PerLevelData[10] = Data.Level11;
+			PerLevelData[11] = Data.Level12;
+			PerLevelData[12] = Data.Level13;
+			PerLevelData[13] = Data.Level14;
+			PerLevelData[14] = Data.Level15;
+			PerLevelData[15] = Data.Level16;
+			PerLevelData[16] = Data.Level17;
+			PerLevelData[17] = Data.Level18;
+			PerLevelData[18] = Data.Level19;
+			PerLevelData[19] = Data.Level20;
 		}
-
 	}
-
-
 }
 
 // holds things we wanna save :)
 [Serializable]
 class GameData
 {
-
+	public LevelScores Level1;
+	public LevelScores Level2;
+	public LevelScores Level3;
+	public LevelScores Level4;
+	public LevelScores Level5;
+	public LevelScores Level6;
+	public LevelScores Level7;
+	public LevelScores Level8;
+	public LevelScores Level9;
+	public LevelScores Level10;
+	public LevelScores Level11;
+	public LevelScores Level12;
+	public LevelScores Level13;
+	public LevelScores Level14;
+	public LevelScores Level15;
+	public LevelScores Level16;
+	public LevelScores Level17;
+	public LevelScores Level18;
+	public LevelScores Level19;
+	public LevelScores Level20;
 }
