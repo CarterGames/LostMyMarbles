651ebb7d3792021f3aeb54b37b4b67cc055c346f
Jonathan <jmc@devj.uk>
2019-07-18 14:57:46 +0100

    New Nav Mesh for Zomballs

--- Assets/Scripts/ZomballScript.cs
+++ Assets/Scripts/ZomballScript.cs
@@ -1,22 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class ZomballScript : MonoBehaviour
 {
 	public GameObject Target;
 	public float ZomballSpd;
 	public bool Enabled;
+	public NavMeshAgent Agent;
 
     void Update()
     {
 		if (Enabled)
 		{
-			if (Vector3.Distance(transform.position, Target.transform.position) > 2)
-			{
-				Vector3 Direction = Target.transform.position - transform.position;
-				GetComponent<Rigidbody>().velocity += Direction * ZomballSpd * Time.deltaTime;
-			}
+			Agent.SetDestination(Target.transform.position);
 		}
 	}
 }
