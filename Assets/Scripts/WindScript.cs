30d7fb7151f8fd3ae8aa7d4fa9b7fc0b9ff227d8
Jonathan <jmc@devj.uk>
2019-07-19 13:02:40 +0100

    More work on the fan mechanic.

--- Assets/Scripts/WindScript.cs
+++ Assets/Scripts/WindScript.cs
@@ -9,13 +9,39 @@ public class WindScript : MonoBehaviour
 	public float Force;
 
 
+	[Header("Fan Rotation Controls")]
+	[Tooltip("The Fan Top Area Object")]
+	public GameObject FanTop;
 
-	private void OnTriggerEnter(Collider other)
+	[Tooltip("Should the fan top rotate?")]
+	public bool RotateFanTop;
+
+	[Tooltip("Amount the rotationg happens")]
+	public float RotationAmount;
+
+	private Vector3 StartRot;
+
+
+	private void Start()
+	{
+		StartRot = new Vector3(FanTop.transform.rotation.x, FanTop.transform.rotation.y, FanTop.transform.rotation.z);
+	}
+
+	private void Update()
+	{
+		if (RotateFanTop && (StartRot.y > RotationAmount))
+		{
+			FanTop.transform.Rotate(-RotationAmount, 0, 0);
+		}
+	}
+
+
+	private void OnTriggerStay(Collider other)
 	{
 		if (other.gameObject.tag == "Player")
 		{
 			Debug.Log("test");
-			other.gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * Force);
+			other.gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * Force, ForceMode.Acceleration);
 		}
 	}
 }
