1817d2ed69dd397ac8b3d15b816183bd66732f3f
Jonathan <jmc@devj.uk>
2019-07-23 14:03:38 +0100

    Started Disco Floor

---
+++ Assets/Scripts/DiscoScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiscoScript : MonoBehaviour
+{
+
+
+	public List<Color32> Colours;
+
+	public List<Material> FloorPieces;
+
+	public float ColourChange;
+
+	private bool IsCoRunning;
+
+    void Start()
+    {
+		for (int i = 0; i < GetComponentsInChildren<MeshRenderer>().Length; i++)
+		{
+			FloorPieces.Add(GetComponentsInChildren<MeshRenderer>()[i].material);
+		}
+		
+		
+    }
+
+
+    void Update()
+    {
+		if (!IsCoRunning)
+		{
+			StartCoroutine(DanceFloorLights());
+		}
+    }
+
+
+	private IEnumerator DanceFloorLights()
+	{
+		for (int i = 0; i < FloorPieces.Count; i++)
+		{
+			FloorPieces[i].color = RandomColour();
+		}
+
+		yield return new WaitForSeconds(ColourChange);
+	}
+
+
+
+	private Color32 RandomColour()
+	{
+		int RandomNumber = Random.Range(0, 9);
+		return Colours[RandomNumber];
+	}
+}
